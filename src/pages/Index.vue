6130eac9eb50f353695f674ad3e457d1a80c8266
Himanshu Gupta <himanshu.gupta@aspiringminds.in>
2021-03-04 23:43:19 +0530

    Branding

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,13 @@
 <template>
   <Layout :sidebar="false">
     <div class="content">
-      <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
+      <h1 class="home-heading">{{ $static.metadata.siteName }} - {{ this.description }}</h1>
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
         <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
         <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
         <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
       </nav>
-      <GitLink class="git" size="large" />
     </div>
   </Layout>
 </template>
